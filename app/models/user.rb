bf7b47fe25ae3a86086270e35fde494c7190019b
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-11-17 16:30:22 +0900

    usertest

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
 end
