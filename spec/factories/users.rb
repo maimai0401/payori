4f88741745a023190d94e48be08b35669587bf6a
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-11-18 15:00:43 +0900

    factories修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname { "satou" }
+    nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
     password { "a12345" }
     password_confirmation { password }
