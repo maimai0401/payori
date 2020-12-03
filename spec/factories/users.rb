23ae6c1b354a3910bf6e9da6b61d4a7e7cf2a14f
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-12-03 15:31:55 +0900

    テスト修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password { "a12345" }
+    password { "1a" + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
   end
 end
