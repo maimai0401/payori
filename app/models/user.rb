57b85ebe14a712fcaa8683e7dc21b17f199e5352
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-12-29 18:57:53 +0900

    アソシエーション 記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :user_address
+  has_one :user_profile
+
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
 end
